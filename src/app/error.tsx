3c126f7fa5a2b9174cb12dd7a73ed0c2a57b5d3c
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2024-09-03 12:34:59 -0400

    Initial next project with chakra ui

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client"; // Error components must be Client Components
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Uh oh
+    </main>
+  );
+}

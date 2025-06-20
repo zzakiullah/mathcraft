8c199eec055e55ec93a8463618ab188c82855580
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2025-06-20 13:30:37 -0400

    Show axis labels when out of bounds

--- src/lib/math/grid.ts
+++ src/lib/math/grid.ts
@@ -2,24 +2,28 @@
  * Functions for handling the grid in the canvas
  */
 
+const BLACK = "#000";
+const GREY = "#979797";
+const DARK_GREY = "#535353";
+
 const drawAxisLines = (
   ctx: CanvasRenderingContext2D,
   width: number,
   height: number,
-  incrementX: number,
-  incrementY: number,
+  gridIncrementX: number,
+  gridIncrementY: number,
   originX: number,
   originY: number,
 ) => {
   // Vertical lines
-  for (let i = incrementX; originX + i < width; i += incrementX) {
+  for (let i = gridIncrementX; originX + i < width; i += gridIncrementX) {
     if (originX + i < 0) {
       continue;
     }
     ctx.moveTo(originX + i, 0);
     ctx.lineTo(originX + i, height);
   }
-  for (let i = incrementX; originX - i > 0; i += incrementX) {
+  for (let i = gridIncrementX; originX - i > 0; i += gridIncrementX) {
     if (originX - i > width) {
       continue;
     }
@@ -28,14 +32,14 @@ const drawAxisLines = (
   }
 
   // Horizontal lines
-  for (let i = incrementY; originY + i < height; i += incrementY) {
+  for (let i = gridIncrementY; originY + i < height; i += gridIncrementY) {
     if (originY + i < 0) {
       continue;
     }
     ctx.moveTo(0, originY + i);
     ctx.lineTo(width, originY + i);
   }
-  for (let i = incrementY; originY - i > 0; i += incrementY) {
+  for (let i = gridIncrementY; originY - i > 0; i += gridIncrementY) {
     if (originY - i > height) {
       continue;
     }
@@ -44,11 +48,122 @@ const drawAxisLines = (
   }
 };
 
-const renderAxisValue = (ctx: CanvasRenderingContext2D, value: number, x: number, y: number) => {
+const renderAxisValue = (
+  ctx: CanvasRenderingContext2D,
+  value: number,
+  x: number,
+  y: number,
+  fillStyle: string,
+) => {
   ctx.strokeStyle = "white";
   ctx.strokeText(`${value}`, x, y);
-  ctx.fillStyle = "black";
+  ctx.fillStyle = fillStyle;
   ctx.fillText(`${value}`, x, y);
+};
+
+const renderAxisValues = (
+  ctx: CanvasRenderingContext2D,
+  labelOffset: number,
+  width: number,
+  height: number,
+  gridIncrementX: number,
+  gridIncrementY: number,
+  incrementX: number,
+  incrementY: number,
+  originX: number,
+  originY: number,
+) => {
+  // Vertical labels
+  ctx.textAlign = "right";
+  ctx.textBaseline = "middle";
+
+  let verticalLabelFillStyle = BLACK;
+  let verticalLabelPositionX = originX - labelOffset;
+
+  // Left out of bounds
+  if (originX - labelOffset < 0) {
+    ctx.textAlign = "left";
+    verticalLabelFillStyle = DARK_GREY;
+    verticalLabelPositionX = labelOffset;
+  }
+  // Right out of bounds
+  else if (originX - labelOffset > width) {
+    verticalLabelFillStyle = DARK_GREY;
+    verticalLabelPositionX = width - labelOffset;
+  }
+
+  // Negative values
+  for (let i = 1; originY + i * gridIncrementY < height; i++) {
+    if (originY + i * gridIncrementY < 0) {
+      continue;
+    }
+    renderAxisValue(
+      ctx,
+      i * -incrementY,
+      verticalLabelPositionX,
+      originY + i * gridIncrementY,
+      verticalLabelFillStyle,
+    );
+  }
+  // Positive values
+  for (let i = 1; originY - i * gridIncrementY > 0; i++) {
+    if (originY - i * gridIncrementY > height) {
+      continue;
+    }
+    renderAxisValue(
+      ctx,
+      i * incrementY,
+      verticalLabelPositionX,
+      originY - i * gridIncrementY,
+      verticalLabelFillStyle,
+    );
+  }
+
+  // Horizontal labels
+  ctx.textAlign = "center";
+  ctx.textBaseline = "top";
+
+  let horizontalLabelFillStyle = BLACK;
+  let horizontalLabelPositionY = originY + labelOffset;
+
+  // Up out of bounds
+  if (originY + labelOffset < 0) {
+    horizontalLabelFillStyle = DARK_GREY;
+    horizontalLabelPositionY = labelOffset;
+  }
+  // Down out of bounds
+  else if (originY + labelOffset > height) {
+    ctx.textBaseline = "bottom";
+    horizontalLabelFillStyle = DARK_GREY;
+    horizontalLabelPositionY = height - labelOffset;
+  }
+
+  // Positive values
+  for (let i = 1; originX + i * gridIncrementX < width; i++) {
+    if (originX + i * gridIncrementX < 0) {
+      continue;
+    }
+    renderAxisValue(
+      ctx,
+      i * incrementX,
+      originX + i * gridIncrementX,
+      horizontalLabelPositionY,
+      horizontalLabelFillStyle,
+    );
+  }
+  // Negative values
+  for (let i = 1; originX - i * gridIncrementX > 0; i++) {
+    if (originX + i * gridIncrementX > width) {
+      continue;
+    }
+    renderAxisValue(
+      ctx,
+      i * -incrementX,
+      originX - i * gridIncrementX,
+      horizontalLabelPositionY,
+      horizontalLabelFillStyle,
+    );
+  }
 };
 
 export const clearGrid = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
@@ -72,7 +187,7 @@ export const createGrid = (
 
   ctx.beginPath();
   ctx.globalAlpha = 1;
-  ctx.strokeStyle = "#979797";
+  ctx.strokeStyle = GREY;
   ctx.lineWidth = 0.5;
 
   // Minor axis lines
@@ -88,13 +203,20 @@ export const createGrid = (
   ctx.stroke();
   ctx.beginPath();
 
+  const showVerticalOriginAxis = originX > -labelOffset && originX < width + labelOffset;
+  const showHorizontalOriginAxis = originY > -labelOffset && originY < height + labelOffset;
+
   // Center axis
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = BLACK;
   ctx.lineWidth = 1.5;
-  ctx.moveTo(originX, 0);
-  ctx.lineTo(originX, height);
-  ctx.moveTo(0, originY);
-  ctx.lineTo(width, originY);
+  if (showVerticalOriginAxis) {
+    ctx.moveTo(originX, 0);
+    ctx.lineTo(originX, height);
+  }
+  if (showHorizontalOriginAxis) {
+    ctx.moveTo(0, originY);
+    ctx.lineTo(width, originY);
+  }
 
   ctx.stroke();
   ctx.beginPath();
@@ -102,25 +224,40 @@ export const createGrid = (
   // Origin axis label
   ctx.lineWidth = 4;
   ctx.font = "14px sans-serif";
-  ctx.textAlign = "right";
-  ctx.textBaseline = "top";
-  renderAxisValue(ctx, 0, originX - labelOffset, originY + labelOffset);
-
-  // Vertical axis labels
-  ctx.textAlign = "right";
-  ctx.textBaseline = "middle";
-  for (let i = 1; i * majorGridSizeX < height; i++) {
-    renderAxisValue(ctx, i * -incrementY, originX - labelOffset, originY + i * majorGridSizeX);
-    renderAxisValue(ctx, i * incrementY, originX - labelOffset, originY - i * majorGridSizeX);
+  if (showVerticalOriginAxis && showHorizontalOriginAxis) {
+    ctx.textAlign = "right";
+    ctx.textBaseline = "top";
+    renderAxisValue(ctx, 0, originX - labelOffset, originY + labelOffset);
   }
 
-  // Horizontal axis labels
-  ctx.textAlign = "center";
-  ctx.textBaseline = "top";
-  for (let i = 1; i * majorGridSizeY < width; i++) {
-    renderAxisValue(ctx, i * incrementX, originX + i * majorGridSizeY, originY + labelOffset);
-    renderAxisValue(ctx, i * -incrementX, originX - i * majorGridSizeY, originY + labelOffset);
-  }
+  renderAxisValues(
+    ctx,
+    labelOffset,
+    width,
+    height,
+    majorGridSizeX,
+    majorGridSizeY,
+    incrementX,
+    incrementY,
+    originX,
+    originY,
+  );
+
+  // // Vertical axis labels
+  // ctx.textAlign = "right";
+  // ctx.textBaseline = "middle";
+  // for (let i = 1; i * majorGridSizeX < height; i++) {
+  //   renderAxisValue(ctx, i * -incrementY, originX - labelOffset, originY + i * majorGridSizeX);
+  //   renderAxisValue(ctx, i * incrementY, originX - labelOffset, originY - i * majorGridSizeX);
+  // }
+
+  // // Horizontal axis labels
+  // ctx.textAlign = "center";
+  // ctx.textBaseline = "top";
+  // for (let i = 1; i * majorGridSizeY < width; i++) {
+  //   renderAxisValue(ctx, i * incrementX, originX + i * majorGridSizeY, originY + labelOffset);
+  //   renderAxisValue(ctx, i * -incrementX, originX - i * majorGridSizeY, originY + labelOffset);
+  // }
 
   ctx.stroke();
 };
